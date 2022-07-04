07408e944e6267b4194bf33ff8c7e18ae53b5d5e
sadaatgithub <sadachaudhari@gmail.com>
2022-07-04 13:35:00 +0530

    created custom user model

--- backend/myapp/models.py
+++ backend/myapp/models.py
@@ -1,3 +1,13 @@
+from operator import truediv
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+
+    class Meta:
+        verbose_name  = 'Chapter'
